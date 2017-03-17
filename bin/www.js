d4e16221175a1a111402e1f1c4b258efbabca6b5
adrianlu95 <alu5@lion.lmu.edu>
2017-03-16 17:19:36 -0700

    changed the file

--- bin/www.js
+++ bin/www.js
@@ -6,6 +6,6 @@ const app = require('../app');
 const port = +process.env.PORT || 8000;
 app.set('port', port);
 
-http.createServer(app).listen(port, () => {
+app.listen(port, () => {
   console.log(`The server is listening on port ${port}`);
 });
