217dbc7f0acdf590132ea2bb6cbff5b76727b9fc
joelhoman <jhoman3@lion.lmu.edu>
2017-04-29 22:17:53 -0700

    added mocha and updated role test

--- test/role_test.js
+++ test/role_test.js
@@ -1,7 +1,12 @@
+const { Role } = require('../server/models');
 const app = require('../app');
 const request = require('supertest');
 
 describe('POST /roles', () => {
+  beforeEach((done) => {
+    Role.truncate({ cascade: true, restartIdentity: true });
+    done();
+  });
   it('responds with a 201 the created role on success', (done) => {
     request(app)
       .post('/roles')
